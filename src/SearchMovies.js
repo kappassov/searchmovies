6cefb27491f99ee74c3568d197454958484aa970
kappassov <abayk0708@gmail.com>
2020-09-26 20:58:50 +0600

    uploading all

---
+++ src/SearchMovies.js
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import MovieCard from "./movieCard.js";
+export default function SearchMovies() {
+  const [query, setQuery] = useState("");
+  const [movies, setMovies] = useState([]);
+  const SearchMovies = async (e) => {
+    e.preventDefault();
+    const url = `https://api.themoviedb.org/3/search/movie?api_key=dfd12afc874455af6508876f64065a40&language=en-US&query=${query}&page=1&include_adult=false`;
+    try {
+      const res = await fetch(url);
+      const data = await res.json();
+      setMovies(data.results);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+  return (
+    <>
+      <form className="form" onSubmit={SearchMovies}>
+        <label className="label" htmlFor="query">
+          Movie Name
+        </label>
+        <input
+          className="input"
+          type="text"
+          name="query"
+          placeholder="i.e. Inception"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <button className="button" type="submit">
+          {" "}
+          Find my Movie
+        </button>
+      </form>
+      <div className="card-list">
+        {movies
+          .filter((movie) => movie.poster_path)
+          .map((movie) => (
+            <MovieCard movie={movie} key={movie.id} />
+          ))}
+      </div>
+    </>
+  );
+}
