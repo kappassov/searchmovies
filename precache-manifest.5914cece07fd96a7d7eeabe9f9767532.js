7e8dcc3d63f8ab79e2eea2dba671ec76ab98e678
kappassov <abayk0708@gmail.com>
2021-07-08 22:56:21 +0600

    Updates

---
+++ precache-manifest.5914cece07fd96a7d7eeabe9f9767532.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb857f653561a5ef1402cacfd631bbd0",
+    "url": "/searchmovies/index.html"
+  },
+  {
+    "revision": "d6058a249ea77b080d4b",
+    "url": "/searchmovies/static/css/main.41eb4479.chunk.css"
+  },
+  {
+    "revision": "993e75358cb9e75cff48",
+    "url": "/searchmovies/static/js/2.3ce302e6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/searchmovies/static/js/2.3ce302e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6058a249ea77b080d4b",
+    "url": "/searchmovies/static/js/main.d1eb73e8.chunk.js"
+  },
+  {
+    "revision": "b6c8862a35f26e3eeaa1",
+    "url": "/searchmovies/static/js/runtime-main.3dee4019.js"
+  }
+]);
